883025a77faa22eed9be6339a05c611f001cde8a
Onfire733 <martineliott9@gmail.com>
2025-12-02 13:40:41 +0100

    Refactor menu and enhance game rules display
    Updated menu function to include a quit option and improved game rules description.

--- affichage_1.c
+++ affichage_1.c
@@ -1,34 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
 int menu(){
 	int choix;
-	do {printf("Bienvenue sur notre jeu [inserer nom] !\n");
-	printf("Que souhaitez vous faire ?\n");
-	printf("\t1 : Lire les regles du jeu \n");
-	printf("\t2 : Lancer une nouvelle partie \n");
-	printf("\t3 : Reprendre votre ancienne partie \n");
-	printf("\t4 : Quitter \n");
-	scanf("%d", &choix);
-	switch(choix){
-		case 1:
-			printf(" regle_jeu()");
-		break;
-		case 2:
-			printf("nouvelle_partie()");
-		break;
-		case 3:
-			printf("reprendre_partie(pseudo)");
-		break;
-	}
-	}while (choix=!4);
+	do {
+		printf("\n");
+		printf("Bienvenue sur notre jeu [inserer nom] !\n");
+		printf("Que souhaitez vous faire ?\n");
+		printf("\t1 : Lire les regles du jeu \n");
+		printf("\t2 : Lancer une nouvelle partie \n");
+		printf("\t3 : Reprendre votre ancienne partie \n");
+		printf("\t4 : Quitter \n");
+		scanf("%d", &choix);
+		switch(choix){
+			case 1:
+				printf("regle_jeu()");
+			break;
+			case 2:
+				printf("nouvelle_partie()");
+			break;
+			case 3:
+				printf("reprendre_partie(pseudo)");
+			break;
+			case 4:
+				printf("Merci d'avoir joué et à bientot !");
+			break;
+		}
+	}while (choix != 4);
 }
+
 void regle_jeu() {
-    printf("patate\n"
-           "chou\n"
+    printf("Ce jeu est de type match 4.\n"
+           "L'objectif est de réaliser des figures afin de faire disparaitre des élèments. Pour cela, \n"
            "carotte\n");
 }
 
 int main (){
 	menu();
+}
+
+int main (){
+	menu();
+
 }

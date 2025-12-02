35de11acbb1ec47b1d1e4f12a62ea4247684332b
tartar7892 <153542011+tartar7892@users.noreply.github.com>
2025-12-02 14:58:04 +0100

    Update affichage_1.c

--- affichage_1.c
+++ affichage_1.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include <wchar.h>
+#include <locale.h>
 
-int menu(){
+void regle_jeu();
+void menu(){
 	int choix;
 	do {
 		printf("\n");
@@ -10,12 +13,12 @@ int menu(){
 		printf("Que souhaitez vous faire ?\n");
 		printf("\t1 : Lire les regles du jeu \n");
 		printf("\t2 : Lancer une nouvelle partie \n");
-		printf("\t3 : Reprendre votre ancienne partie \n");
+		printf("\t3 : Reprendre votre ancienne partie\n");
 		printf("\t4 : Quitter \n");
 		scanf("%d", &choix);
 		switch(choix){
 			case 1:
-				printf("regle_jeu()");
+				regle_jeu();
 			break;
 			case 2:
 				printf("nouvelle_partie()");
@@ -24,29 +27,34 @@ int menu(){
 				printf("reprendre_partie(pseudo)");
 			break;
 			case 4:
-				printf("Merci d'avoir joué et à bientot !");
+				
+				wchar_t texte[] = L"Merci d'avoir joué et à bientot!";
+				wprintf(L"%ls\n", texte);
+
 			break;
 		}
 	}while (choix != 4);
 }
 
 void regle_jeu() {
-    printf("Ce jeu est de type match 4.\n"
-           "L'objectif est de réaliser des figures afin de faire disparaitre des élèments.\n"
-		   "Pour cela, vous pouvez interchanger deux élèments en utilisants les fléches directionelles et entrées pour valider. \n" //les touches ne sont pas fixes
-           "Les figures à réaliser sont les suivantes :\n"
-		   "la croix, avec des branches de deux élèments, qui fait disparaitre tout les élèments de ce type sur la ligne et la colonne concernèes. ;\n"
-		   "le carrré, de coté 4, fait disparaitre tout les élèments du type utilisé à l'intérieur de lui même;\n"
-		   "la ligne de 6 élèments qui fait disparaitre tout les élèments utilisés pour la constituer, même ceux isolés;\n"
-		   "la ligne de 4 élèments qui fait disparaitre tout les élèments utilisés pour la constituer.\n"
-		   "\n"
-		   "Il y a trois niveau, chacun de difficulté croissante.\n"
-		   "Pour les réliser, vous disposez de d'un nombre de coup et d'un temps limité.\n"
-		   "Si vous échouez, vous perdez une vie. Vous disposez de trois vies et si vous les perdez toutes vous recommencez à zéro.\n"
-		   "Vous pouvez sauvegarder à tout moment votre progression.\n");
+    wchar_t text_regle[]=L"\t=== RÈGLES DU JEU ===\n\n"
+       "Ce jeu est de type match 4.\n"
+       "L'objectif est de réaliser des figures afin de faire disparaître des éléments.\n"
+       "Vous pouvez interchanger deux éléments en utilisant les flèches directionnelles et la touche entrée pour valider.\n\n"
+       "Les figures à réaliser sont :\n"
+       "\t- La croix (branches de 2 éléments) : fait disparaître tous les éléments de ce type sur la ligne et la colonne concernées.\n"
+       "\t- Le carré (côté 4) : fait disparaître tous les éléments du type utilisé à l'intérieur.\n"
+       "\t- La ligne de 6 éléments : fait disparaître tous les éléments utilisés pour la constituer, même isolés.\n"
+       "\t- La ligne de 4 éléments : fait disparaître tous les éléments utilisés pour la constituer.\n\n"
+       "Il y a trois niveaux, chacun de difficulté croissante.\n"
+       "Pour les réaliser, vous disposez d'un nombre de coups et d'un temps limité.\n"
+       "Si vous échouez, vous perdez une vie. Vous avez trois vies, et si vous les perdez toutes vous recommencez à zéro.\n"
+       "Vous pouvez sauvegarder votre progression à tout moment.\n";
+	   wprintf(L"%ls\n", text_regle);
 }
 
 int main (){
+	setlocale(LC_ALL, "");
 	menu();
 	return 0;
 }
